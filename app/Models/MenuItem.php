7281d8f827dfa98e448ad1a26195567c1812f813
alibaghernia <alibaghernia0@gmail.com>
2022-12-17 01:35:26 +0330

    feat: add MenuItem's model, category and policy

---
+++ app/Models/MenuItem.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MenuItem extends Model
+{
+    use HasFactory;
+
+    public function menuCategory()
+    {
+        return $this->belongsTo(MenuCategory::class);
+    }
+}
---
+++ database/migrations/2022_11_27_162338_create_menu_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('menu_category_id')->constrained();
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+            $table->string('image_path')->nullable();
+            $table->unsignedInteger('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_items');
+    }
+};

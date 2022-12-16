8fef9a32d45f00e60feff384e63b1385d4b87fce
alibaghernia <alibaghernia0@gmail.com>
2022-12-16 04:39:05 +0330

    customize login page

---
+++ app/Filament/Pages/Auth/Login.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Filament\Http\Livewire\Auth;
+
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+use DanHarrin\LivewireRateLimiting\WithRateLimiting;
+use Filament\Facades\Filament;
+use Filament\Forms\ComponentContainer;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Http\Responses\Auth\Contracts\LoginResponse;
+use Illuminate\Contracts\View\View;
+use Illuminate\Validation\ValidationException;
+use Livewire\Component;
+
+/**
+ * @property ComponentContainer $form
+ */
+class Login extends Component implements HasForms
+{
+    use InteractsWithForms;
+    use WithRateLimiting;
+
+    public $username = '';
+
+    public $password = '';
+
+    public $remember = false;
+
+    public function mount(): void
+    {
+        if (Filament::auth()->check()) {
+            redirect()->intended(Filament::getUrl());
+        }
+
+        $state = ['remember' => true,];
+        if (config('app.debug')) {
+            $state['username'] = 'admin';
+            $state['password'] = 'password';
+        }
+        $this->form->fill($state);
+    }
+
+    public function authenticate(): ?LoginResponse
+    {
+        try {
+            $this->rateLimit(5);
+        } catch (TooManyRequestsException $exception) {
+            throw ValidationException::withMessages([
+                'username' => __('filament::login.messages.throttled', [
+                    'seconds' => $exception->secondsUntilAvailable,
+                    'minutes' => ceil($exception->secondsUntilAvailable / 60),
+                ]),
+            ]);
+        }
+
+        $data = $this->form->getState();
+
+        if (!Filament::auth()->attempt([
+            'username' => $data['username'],
+            'password' => $data['password'],
+        ], $data['remember'])) {
+            throw ValidationException::withMessages([
+                'username' => __('filament::login.messages.failed'),
+            ]);
+        }
+
+        session()->regenerate();
+
+        return app(LoginResponse::class);
+    }
+
+    protected function getFormSchema(): array
+    {
+        return [
+            TextInput::make('username')
+                ->minLength(4)
+                ->required()
+                ->autocomplete(),
+            TextInput::make('password')
+                ->label(__('filament::login.fields.password.label'))
+                ->password()
+                ->required(),
+            Checkbox::make('remember')
+                ->label(__('filament::login.fields.remember.label')),
+        ];
+    }
+
+    public function render(): View
+    {
+        return view('filament::login')
+            ->layout('filament::components.layouts.card', [
+                'title' => __('filament::login.title'),
+            ]);
+    }
+}

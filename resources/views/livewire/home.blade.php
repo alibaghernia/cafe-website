c819a06fa651a5efadd34ed5e5fb87bef7273081
alibaghernia <alibaghernia0@gmail.com>
2022-12-17 01:38:48 +0330

    feat: add home page

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,12 @@
+<div class="mx-auto container">
+    <div class="mt-2 mx-2">
+        <img src="{{ asset('logo.jpeg') }}" alt="" class="rounded-xl shadow mx-auto">
+    </div>
+    <div class="text-center text-xl font-extrabold mt-5">
+        Tourism coffee
+    </div>
+    <div class="text-center px-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla perspiciatis eius omnis molestiae eaque maxime
+        alias libero nobis, nisi voluptas tempore temporibus est commodi impedit? Delectus ullam nisi eligendi fugit.
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', Home::class)
+    ->name('home');
+
 Route::get('blog', Blog::class)
     ->name('blog');
 

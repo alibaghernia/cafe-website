e4d80c07a80c2a1408407929dd0f22d83f6027e3
alibaghernia <alibaghernia0@gmail.com>
2022-12-17 10:11:58 +0330

    feat(database/seeders): add DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Event;
+use App\Models\User;
+use App\Models\Weekday;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +18,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'admin',
+            'username' => 'admin',
+            'password' => '$2y$10$eQ9qkgSWN5Hi4eObCTdGJeOz1B4aZnj/WPla25xfXT9PpCd5Zw.dC', //password
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $SATURDAY = Weekday::create(['name' => Weekday::SATURDAY]);
+        $SUNDAY = Weekday::create(['name' => Weekday::SUNDAY]);
+        $MONDAY = Weekday::create(['name' => Weekday::MONDAY]);
+        $TUESDAY = Weekday::create(['name' => Weekday::TUESDAY]);
+        $WEDNESDAY = Weekday::create(['name' => Weekday::WEDNESDAY]);
+        $THURSDAY = Weekday::create(['name' => Weekday::THURSDAY]);
+        $FRIDAY = Weekday::create(['name' => Weekday::FRIDAY]);
+
+
+        Event::create([
+            'name' => 'Free discussion',
+            'start' => '19:00',
+            'weekday_id' => $SUNDAY->id,
+        ]);
+
+        Event::create([
+            'name' => 'Free discussion',
+            'start' => '19:00',
+            'weekday_id' => $MONDAY->id,
+        ]);
+
+        Event::create([
+            'name' => 'Free discussion',
+            'start' => '19:00',
+            'weekday_id' => $TUESDAY->id,
+        ]);
+
+        Event::create([
+            'name' => 'Free discussion',
+            'start' => '10:30',
+            'weekday_id' => $THURSDAY->id,
+        ]);
+
+        Event::create([
+            'name' => 'Mafia game',
+            'start' => '18:30',
+            'weekday_id' => $FRIDAY->id,
+        ]);
+
+
+
+        $this->call([
+            TestDataSeeder::class,
+        ]);
     }
 }

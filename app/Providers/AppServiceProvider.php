7b6c50a8cb51d6fb20e3acddf45c664ea91168ad
alibaghernia <alibaghernia0@gmail.com>
2023-03-31 00:23:06 +0330

    add defaultStringLength

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Model::unguard();
+        Schema::defaultStringLength(191);
     }
 }

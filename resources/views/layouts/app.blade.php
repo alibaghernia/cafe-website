05838c0161086a4e3ab11aca3a488bdba879d251
alibaghernia <alibaghernia0@gmail.com>
2023-05-20 18:52:03 +0330

    feat(seo): Add Description meta tag to pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="application-name" content="{{ config('app.name') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="@yield('page_description', config('app.name'))">
 
     <title>{{ config('app.name') }} | @yield('page_title')</title>
 

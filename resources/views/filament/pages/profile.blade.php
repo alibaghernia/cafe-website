70adf0805ce42483193a740f0512725126e54fd1
alibaghernia <alibaghernia0@gmail.com>
2023-07-17 01:26:29 +0330

    feat: translate label of  save button in profile page and add styles to the button

--- resources/views/filament/pages/profile.blade.php
+++ resources/views/filament/pages/profile.blade.php
@@ -2,8 +2,13 @@
     <form wire:submit.prevent="submit">
         {{ $this->form }}
 
-        <button type="submit">
-            Submit
+        <button type="submit"
+                class="filament-button filament-button-size-md inline-flex items-center justify-center py-1 gap-1
+                 font-medium rounded-lg border transition-colors outline-none focus:ring-offset-2 focus:ring-2
+                 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-sm text-white shadow
+                 focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700
+                 focus:ring-offset-primary-700 filament-page-button-action mt-4">
+            ذخیره
         </button>
     </form>
 </x-filament::page>
